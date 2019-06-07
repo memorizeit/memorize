5a60840ba12f105a140254b4285d6ad2589e89b7
cledersonbc <cledersonbc@live.com>
2019-06-06 21:05:00 -0300

    NEW: config of spring B008

---
+++ src/main/java/com/memorize/conf/AppWebConfig.java
@@ -0,0 +1,25 @@
+package com.memorize.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+public class AppWebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
---
+++ src/main/java/com/memorize/conf/ServletSpringMVC.java
@@ -0,0 +1,35 @@
+package com.memorize.conf;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+
+		return new Class<?>[] { AppWebConfig.class, JPAConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter("UTF-8");
+
+		return new Filter[] { encodingFilter };
+	}
+
+}
